6ea39db5212a532dfee214455d5330cc318c0ef8
joe <joe@wpj.cz>
2025-02-21 20:16:12 +0100

    First working version

--- grid_movement/character.gd
+++ grid_movement/character.gd
@@ -1,21 +1,21 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * SPEED, delta*1000)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, delta*1000)
 		
 	direction = Input.get_axis("ui_up", "ui_down")
 	if direction:
-		velocity.y = direction * SPEED
+		velocity.y = move_toward(velocity.y, direction * SPEED, delta*1000)
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, delta*1000)
 
 	move_and_slide()
